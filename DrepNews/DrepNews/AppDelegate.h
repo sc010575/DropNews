7d7ca086985e6d8f6794fd0d928d3309960fd3f3
Suman Chatterjee <chatterjeesuman@gmail.com>
2014-11-27 14:35:58 +0000

    project Setup

---
+++ DrepNews/DrepNews/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  DrepNews
+//
+//  Created by Suman Chatterjee on 27/11/2014.
+//  Copyright (c) 2014 Suman Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ DrepNews/DrepNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrepNews
+//
+//  Created by Suman Chatterjee on 27/11/2014.
+//  Copyright (c) 2014 Suman Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
